ef524de7a5edee337e8f07ed94a1f795ed0ba6da
Rachid <ouattchidi@gmail.com>
2019-10-03 00:54:22 +0200

    update example by adding a second instance

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -4,23 +4,24 @@ resource "openstack_compute_keypair_v2" "keypair" {
   name = "my-keypair"
 }
 
-module "mgmt_network" {
-  #source              = "../terraform-os-network/"
+
+module "network" {
   source              = "github.com/dinivas/terraform-openstack-network"
-  network_name        = "test-mgmt"
-  network_tags        = ["test", "management", "dinivas"]
-  network_description = ""
+  network_name        = "my-network"
+  network_tags        = ["management", "dinivas"]
+  network_description = "My network description"
 
   subnets = [
     {
-      subnet_name       = "test-mgmt-subnet"
-      subnet_cidr       = "10.10.17.1/24"
+      subnet_name       = "my-network-subnet"
+      subnet_cidr       = "10.10.14.0/24"
       subnet_ip_version = 4
-      subnet_tags       = "test, management, dinivas"
+      subnet_tags       = "management, dinivas"
     }
   ]
 }
-module "compute" {
+
+module "compute1" {
   source                       = "../../"
   instance_name                = "BLUE"
   instance_count               = 1
@@ -30,6 +31,30 @@ module "compute" {
   network_ids                  = ["${module.network.network_id}"]
   subnet_ids                   = ["${module.network.subnet_ids}"]
   instance_security_group_name = "BLUE-sg"
+  instance_security_group_rules = [
+    {
+      direction        = "ingress"
+      ethertype        = "IPv4"
+      protocol         = "tcp"
+      port_range_min   = 22
+      port_range_max   = 22
+      remote_ip_prefix = ""
+  }]
+  enabled = "1"
+  availability_zone = "nova:node03"
+}
+
+
+module "compute2" {
+  source                       = "../../"
+  instance_name                = "GREEN"
+  instance_count               = 1
+  image_name                   = "cirros"
+  flavor_name                  = "m1.tiny"
+  keypair                      = "${openstack_compute_keypair_v2.keypair.name}"
+  network_ids                  = ["${module.network.network_id}"]
+  subnet_ids                   = ["${module.network.subnet_ids}"]
+  instance_security_group_name = "GREEN-sg"
   instance_security_group_rules = [
     {
       direction        = "ingress"
--- examples/simple/outputs.tf
+++ examples/simple/outputs.tf
@@ -1,9 +1,19 @@
-output "ids" {
+output "compute1_ids" {
   description = "List of IDs of instances"
-  value       = "${module.compute.ids}"
+  value       = "${module.compute1.ids}"
 }
 
-output "names" {
+output "compute1_names" {
   description = "List of IDs of instances"
-  value       = "${module.compute.names}"
+  value       = "${module.compute1.names}"
+}
+
+output "compute2_ids" {
+  description = "List of IDs of instances"
+  value       = "${module.compute2.ids}"
+}
+
+output "compute2_names" {
+  description = "List of IDs of instances"
+  value       = "${module.compute2.names}"
 }
